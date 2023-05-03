96f4801829ee1d287887165b7cf5efbf232fda38
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-03 17:13:34 +0200

    productList

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,33 @@
-<div>Store</div>
+<script lang="ts">
+    import { onMount } from "svelte"
+
+
+    interface Product{
+        id: number;
+        title: string;
+
+    }
+
+    let productList: Product[] = []
+
+    async function fetchProduct(){
+        const response = await fetch("https://fakestoreapi.com/products")
+        const data = await response.json()
+        productList = data
+    }
+
+    onMount(() => {
+        fetchProduct();
+    })
+
+
+</script>
+
+
+
+<h1>Storwwe page.svelte</h1>
+<ul>
+    {#each productList as product}
+    <li>{product.id}{'--->'}{product.title}</li>
+    {/each}
+</ul>
