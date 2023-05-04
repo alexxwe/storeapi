e8e1faeb8f0b1ffa29c3b51d61067f4c0128d085
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-04 13:30:18 +0200

    import app and web view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
+    import "../app.css"
     import type { ProductListDto } from "$lib/types/productList.dto"
     import { onMount } from "svelte"
 
-
-    
 
     let productList: ProductListDto[] = []
 
@@ -20,11 +19,15 @@
 
 </script>
 
+<!-- max-w-7xl = 80rem (1280px)  -::::-  max-w-screen-2xl = (1536px)-->
+<div class="mx-auto max-w-screen-2xl">  
+    <h1 class="text-center font-bold text-3xl">Storwwe page.svelte</h1>
+    <ul class="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
+        {#each productList as product}
+        <li class="rounded-lg bg-zinc-400 p-4 text-center" >{product.id}{'--->'}{product.title}</li>
+        {/each}
+    </ul>
+</div>
+    
 
 
-<h1>Storwwe page.svelte</h1>
-<ul>
-    {#each productList as product}
-    <li>{product.id}{'--->'}{product.title}</li>
-    {/each}
-</ul>
