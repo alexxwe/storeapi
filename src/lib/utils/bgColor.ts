9ab474a5e028813ffe44ef9c35175109eb803e07
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-08 16:25:37 +0200

    category color

---
+++ src/lib/utils/bgColor.ts
@@ -0,0 +1,6 @@
+export const bgColor: Record<string, string> = {
+    "men's clothing": 'bg-blue-400',
+    "jewelery": 'bg-gray-400',
+    "electronics": 'bg-amber-400',
+    "women's clothing": 'bg-rose-400',
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import "../app.css"
     import type { ProductListDto } from "$lib/types/productList.dto"
+    import { bgColor } from "$lib/utils/bgColor"
 
     /** @type {import('./$types').PageData} */
     export let data: {
@@ -12,9 +13,14 @@
 <!-- max-w-7xl = 80rem (1280px)  -::::-  max-w-screen-2xl = (1536px)-->
 <div class="mx-auto max-w-screen-2xl">  
     <h1 class="text-center font-bold text-3xl">Storwwe page.svelte</h1>
-    <ul class="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 mx-auto">
+    <ul class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
         {#each data.products as product}
-        <li class="rounded-lg bg-zinc-400 p-4 text-center" >{product.id}{'--->'}{product.title}</li>
+        <li class="rounded-lg p-4 text-center {bgColor[product.category]} " >id{product.id}<br>
+            title{product.title}<br>
+            category{product.category}<br>
+            description{product.description}<br>
+            price{product.price}<br>
+            rating{product.rating}</li>
         {/each}
     </ul>
 </div>
