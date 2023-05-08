bc4f76490d807abd54411a816dd14b5942919aeb
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-08 17:08:58 +0200

    change file name

--- src/lib/utils/bgColor.ts
+++
@@ -1,6 +0,0 @@
-export const bgColor: Record<string, string> = {
-    "men's clothing": 'bg-blue-400',
-    "jewelery": 'bg-gray-400',
-    "electronics": 'bg-amber-400',
-    "women's clothing": 'bg-rose-400',
-}
