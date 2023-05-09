bfb5937c9497a245e9ed51ac364d21be958d68ca
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-09 14:53:25 +0200

    product.svelte
    product card implementation

---
+++ src/lib/components/Product.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { ProductListDto } from "$lib/types/productList.dto"
+    import { bgColor, shadowColor } from "$lib/utils/colors"
+
+    export let product: ProductListDto
+
+
+</script>
+
+<!-- max-w-7xl = 80rem (1280px)  -::::-  max-w-screen-2xl = (1536px)-->
+<div class="flex">  
+        <li class="rounded-lg p-4 text-center {bgColor[product.category]} {shadowColor[product.category]}
+            animate__animated animate__fadeInDown p-4 text-center">
+                <p class="text-xl my-2"># {product.id} {product.title}<br></p>
+                <p class="font-bold text-black my-2">{product.category.toUpperCase()}<br></p>
+                <img src={product.image} alt={product.title} class=" h-40 my-4 mx-auto ">
+                <p class="my-2">description{product.description}<br></p>
+                <p class="">price{product.price}<br></p>
+        </li>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import "../app.css"
     import type { ProductListDto } from "$lib/types/productList.dto"
     import { bgColor, shadowColor } from "$lib/utils/colors"
+    import Product from "$lib/components/Product.svelte"
 
     /** @type {import('./$types').PageData} */
     export let data: {
@@ -15,13 +16,7 @@
     <h1 class="text-center font-bold text-4xl p-8">Storwwe page.svelte</h1>
     <ul class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mx-auto">
         {#each data.products as product}
-        <li class="rounded-lg p-4 text-center {bgColor[product.category]} {shadowColor[product.category]}">
-                <p class="text-xl my-2"># {product.id} {product.title}<br></p>
-                <p class="font-bold text-black my-2">{product.category.toUpperCase()}<br></p>
-                <img src={product.image} alt={product.title} class=" h-40 my-4 mx-auto">
-                <p class="my-2">description{product.description}<br></p>
-                <p class="">price{product.price}<br></p>
-        </li>
+        <Product {product} />
         {/each}
     </ul>
 </div>
