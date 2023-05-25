52c811d8c01cbd4c9eae4cc383e81398677771f8
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-05-25 16:15:49 +0200

    Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    
+</script>
+
+<header class="my-2 mx-4 font-bold flex items-center justify-between" >
+    <a href="../" rel="icon" target="_self"><i class=" text-4xl bi bi-box-fill"></i><br>Home</a>
+    <h1 class="flex justify-center text-4xl">Storwwe</h1>
+    <a href="https://pokeapi-alexxpf.vercel.app/" rel="noreferrer" target="_blank"><i class="text-4xl bi bi-record-circle"></i><br>Poke</a>
+</header>
