917d045a409ecfaf3bda00a5de39f8b4ea249ccf
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-06-08 20:20:39 +0200

    Add to Cart

--- src/routes/product/[slug]/+page.svelte
+++ src/routes/product/[slug]/+page.svelte
@@ -29,6 +29,23 @@ const back = () => {
     index = index === 0 ? maxIndex : index - 5
   }
 
+
+  let cartItems: ProductListDto[] = [];
+
+
+  const addToCart = () => {
+    const product: ProductListDto = {
+      id: data.product.id,
+      title: data.product.title,
+      price: data.product.price,
+      image: data.product.image,
+      description: data.product.description,
+      category: data.product.category,
+      rating: data.product.rating,
+    };
+    cartItems = [...cartItems, product];
+  }
+
 </script>
 
 
@@ -53,15 +70,16 @@ const back = () => {
             <span class="text-black font-bold text-4xl">{data.product.price}€</span>
           </div>
           
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold  text-xl py-2 px-4 rounded">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold  text-xl py-2 px-4 rounded" on:click={addToCart}>
             Add to Cart
           </button>
+          
         </div>
       </div>
     </div>
     
 <div class="mt-8 bg-white rounded">
-  <h2 class="text-4xl text-black font-bold mb-4">Related Products</h2>          
+  <h2 class="text-4xl text-black font-bold mb-4 ml-2">Related Products</h2>          
   <div class="m-4 flex mx-auto bg-white rounded">
       <ul class="grid grid-cols-5 gap-4 mx-4 my-4">
           {#each data.products.slice(index, index + 5) as product }
@@ -75,14 +93,28 @@ const back = () => {
           </a>
           {/each}
       </ul>
-  </div>
-  <div>
-      <button class="px-8 py-2 text-white bg-blue-500 rounded" on:click={back}>
+    </div>
+    <div class="flex justify-center gap-2 mb-4" >
+      <div class="mb-2">
+        <button class="px-8 py-2 text-white bg-blue-500 rounded" on:click={back}>
           Back
-      </button>
-      <button class="px-8 py-2 text-white bg-blue-500 rounded" on:click={next}>
+        </button>
+        <button class="px-8 py-2 text-white bg-blue-500 rounded" on:click={next}>
           Next
-  </button>
+        </button>
+      </div>
+  </div>
 </div>
+<div class="text-black fixed mt-20 top-4 right-4 m-4 p-2 bg-white rounded shadow">
+  <span class="text-xl font-bold">Cart: {cartItems.length}</span>
+  <div class="mt-2">
+    {#each cartItems as item (item.id)}
+      <div class="flex items-center mb-2 border-b border-gray-300 pb-2">
+        <img class="w-12 h-12 mr-2 object-contain rounded" src={item.image} alt={item.title} />
+        <p class="underline">{item.price}€</p>
+        <p class="text-md truncate overflow-ellipsis max-w-[20ch] ml-2">{item.title}</p>
+      </div>
+    {/each}
+  </div>
 </div>
 </main>
