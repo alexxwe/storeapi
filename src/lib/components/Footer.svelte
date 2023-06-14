f7e137fde5097abc60f9220ef24a7ea195817a74
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-06-14 23:56:12 +0200

    footer update link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="flex justify-between my-4 mx-4 text-2xl ">
     <a href="https://es.linkedin.com/" rel="noreferrer" target="_blank"><i class="bi bi-linkedin"></i> Linkedin</a>
     <a href="https://twitter.com/" rel="noreferrer" target="_blank"> <i class="bi bi-twitter"></i> Twitter</a>
-    <a href="https://github.com/" rel="noreferrer" target="_blank"><i class="bi bi-github"></i> GitHub</a>
+    <a href="https://github.com/alexxwe" rel="noreferrer" target="_blank"><i class="bi bi-github"></i> GitHub</a>
     <a href="https://platzi.com/" rel="noreferrer" target="_blank"><i class="bi bi-star-fill"></i> Platzi</a>
 
 </footer>
